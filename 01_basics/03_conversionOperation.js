d611369ecf76c3f49195b9637ac3602002543df4
Sukhpreet Singh <sukhpreetsingh19289@gmail.com>
2025-04-04 16:21:53 +0000

    changes saved

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,14 +12,14 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "sukhpreet"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "sukhpreet" => true
 
 let someNumber = 33
 
